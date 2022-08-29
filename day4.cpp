eb1b037759d77d3be51d07b6e05a69ffc329d135
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-08-29 22:37:11 +0530

    Add files via upload
    day 4 out of 100

---
+++ day4.cpp
@@ -0,0 +1,19 @@
+// cpp program to print the string in reverse
+// day 4 out of 100
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void reversestr(string str){
+    for(int i = str.length();i>=0;i--){  // running the for loop in reverse
+        cout<<str[i];                   // printing it
+    }
+}
+
+int main()
+{
+    string s = "Tharunn"; // creating  a string
+    reversestr(s);   // calling the function
+    return 0;
+}
