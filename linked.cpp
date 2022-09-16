e676096ef7e351a6e8be0bb9a28fcea2226b9c55
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-09-16 23:22:56 +0530

    Add files via upload

---
+++ linked.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class Node{    
+public:
+    int data;
+    Node* next;
+};
+
+void printnode(Node* n){
+
+    while(n!=NULL){
+        cout<<n->data<<' ';
+        n=n->next;
+    }
+}
+
+int main(){
+
+    Node* head = NULL;  // node is a datatype which takes the address and stores in head
+    Node* two = NULL;   // 
+    Node* three = NULL;
+
+    head = new Node();
+    two = new Node();
+    three = new Node();
+
+    head->data = 1;
+    head->next = two;
+
+    two->data = 2;
+    two->next = three;
+
+    three->data = 3;
+    three->next = NULL;
+
+    printnode(head);
+    return 0;
+}
