e14cc1c484fcedca67e6be2427657b7a9eed86c0
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-09-19 23:50:01 +0530

    Create gfg average in stream.cpp

---
+++ gfg average in stream.cpp
@@ -0,0 +1,15 @@
+//User function template for C++
+
+class Solution{
+public:	
+	vector<float> streamAvg(int arr[], int n) {
+	    // code here
+	    vector<float> avg;
+	    float sum =0;
+	    for(int i=0;i<n;i++){
+	        sum += arr[i];
+	         avg.push_back(sum/(i+1));
+	    }
+	  return avg; 
+	}
+};
