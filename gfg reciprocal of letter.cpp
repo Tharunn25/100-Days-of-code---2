4c8e3ef529813b47097c494b3ba0494b92ad1200
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-09-25 23:14:42 +0530

    Create gfg reciprocal of letter.cpp

---
+++ gfg reciprocal of letter.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    string reciprocalString(string s)
+    {
+        // Write Your code here
+        for(int i=0;i<s.size();i++){
+            
+            if(isupper(s[i]))
+               s[i] = 'Z' - (s[i]-'A');
+             
+            else if(islower(s[i]))
+               s[i] = 'z' - (s[i]-'a');
+            else
+               s[i]=s[i];
+        }
+        return s;
+    }
+};
