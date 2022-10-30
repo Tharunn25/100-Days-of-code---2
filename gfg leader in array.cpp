47db5a7f94fdaa834fab96c7ec5e4bc5eade9e46
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-10-30 11:19:28 +0530

    Create gfg leader in array.cpp

---
+++ gfg leader in array.cpp
@@ -0,0 +1,41 @@
+class Solution{
+
+    //Function to find the leaders in the array.
+
+    public:
+
+    vector<int> leaders(int a[], int n){
+
+        // Code here
+
+        vector<int> vec;
+
+      int max = a[n-1];
+
+      vec.push_back(max);
+
+        for(int i=n-2;i>=0;i--)
+
+        {
+
+          if(a[i]>=max ) 
+
+         {
+
+             max=a[i];
+
+             vec.push_back(a[i]);
+
+         }
+
+        }
+
+        reverse(vec.begin(), vec.end());
+
+        return vec;
+
+        
+
+    }
+
+};
