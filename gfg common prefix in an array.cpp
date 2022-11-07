40ccffb8ef3d9bdf6c3b5879deebbf6692e6f750
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-11-07 23:46:08 +0530

    Create gfg common prefix in an array.cpp

---
+++ gfg common prefix in an array.cpp
@@ -0,0 +1,22 @@
+
+class Solution{
+  public:
+    
+    string longestCommonPrefix (string arr[], int N)
+    {
+        // your code here
+         string x = arr[0];
+
+        for(int i=1;i<N;i++){
+
+            while(arr[i].find(x)){
+
+                x.pop_back();
+
+            }
+
+        }
+
+        return (x == "")?"-1":x;
+    }
+};
