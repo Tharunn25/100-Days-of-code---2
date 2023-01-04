9dcc4a6e731a6875917f970de8488a27da3a3a65
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2023-01-04 23:58:43 +0530

    Create Richest customer.cpp

---
+++ Richest customer.cpp
@@ -0,0 +1,10 @@
+int sum = 0;
+        int mx = INT_MIN;
+        for(int i=0; i<accounts.size(); i++){
+            sum = 0;
+            for( int j=0; j<accounts[0].size();j++){
+                sum+=accounts[i][j];
+            }
+            mx= max(mx,sum);
+        }
+        return mx;
