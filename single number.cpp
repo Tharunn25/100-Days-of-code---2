320403106d04da42e7755175e9cb99ec51c20ce2
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2023-02-04 14:58:20 +0530

    Create single number.cpp

---
+++ single number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0; // declaring ans as zero
+        for(auto x:nums)   // iterating the arr with x as iteration
+        {
+            ans = ans^x;  // we are using bitwise operator if we have same elements it will return 0 but we need the different element from the other so it will return the ans
+        }
+        return ans;
+    }
+};
