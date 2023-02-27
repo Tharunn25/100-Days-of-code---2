d6d0f284ff628128784201ed5f1c58910955d2e7
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2023-02-27 13:14:51 +0530

    Create unique morse code words.cpp

---
+++ unique morse code words.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        vector<string>a{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+        set<string> s;
+
+        for(int i=0;i<words.size();i++)
+        {
+            string p = " ";
+            for(int j=0;j<words[i].size();j++)
+            {
+                p += a[words[i][j]-'a'];
+            }
+            s.insert(p);
+        }
+        return size(s);
+        
+    }
+};
