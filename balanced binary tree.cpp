6a7395c668c68d98df3a98b625ce5d1a5e695f17
who Tf is Tharunn <98308855+Tharunn25@users.noreply.github.com>
2023-04-05 21:10:04 +0530

    Update balanced binary tree.cpp

--- balanced binary tree.cpp
+++ balanced binary tree.cpp
@@ -11,12 +11,23 @@
  */
 class Solution {
 public:
-    int maxDepth(TreeNode* root) {
-         if(root==NULL) return  0;
-         int lh = maxDepth(root->left);
-         int rh = maxDepth(root->right);
+    int height(TreeNode* root)
+    {
+        if(root==NULL) return 0;  // if we get null in the root we are returning 0 
+        int lh = height(root->left);  // declaring left height and pointing it to root's left
+        if(lh == -1) return -1;      // if root's height is -1 we will return -1
+        int rh = height(root->right);  // declaring right height and pointing it to root's right
+        if(rh == -1) return -1;        // if root's height is -1 we will return -1
+        
+        if(abs(lh-rh)>1) return -1;    // if the absoulte value is also - 1 which means the tree is not balance so again we will return -1
 
-         return 1 + max(lh,rh);
+        return max(lh,rh)+1;  // atlast we will return max of left and right + 1   
+    } 
+    
+    bool isBalanced(TreeNode* root) {
+         
+         return height(root) != -1;  // we are calling the above height function, by passing root and will return false if -1 occurs
 
+        
     }
 };
