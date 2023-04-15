5e857173028781ecf2d13a19b44db2a0d58051e7
who Tf is Tharunn <98308855+Tharunn25@users.noreply.github.com>
2023-04-15 23:05:15 +0530

    Update single number.cpp

--- single number.cpp
+++ single number.cpp
@@ -7,5 +7,9 @@ public:
             ans = ans^x;  // we are using bitwise operator if we have same elements it will return 0 but we need the different element from the other so it will return the ans
         }
         return ans;
+//yfufufu
+
+
+
     }
 };
